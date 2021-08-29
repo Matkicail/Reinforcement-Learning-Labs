78e3a5382f6492dde65e40cb661586252bc415d5
mikayla-anne <61417406+mikayla-anne@users.noreply.github.com>
2021-08-29 11:15:09 +0200

    all done!

--- Lab 3/lab3.py
+++ Lab 3/lab3.py
@@ -18,15 +18,12 @@ class GridWorld():
         self.map = np.zeros((rows, cols))
         self.rows = rows
         self.cols = cols
-        # define the value map
-        self.valueMap = np.zeros((rows,cols))
-        # denote an obstacle with a sentinel value
-        self.obstacle = -9
 
     # Returns the grid and it's dimensions
     def getEnvironmentMap(self):
         return self.map, self.rows, self.cols
 
+# Returns the next possible states based on all actions
 def getNextStates(gridMap,i,j):
     
     n = gridMap.shape[0]
@@ -43,6 +40,7 @@ def getNextStates(gridMap,i,j):
     
     return next
 
+# Returns the rewards for moving on the given states
 def getRewards(rewards,states):
     re = []
     
@@ -52,6 +50,7 @@ def getRewards(rewards,states):
         
     return re
 
+# Returns the values of states given
 def getValue(V,states):
     val = []
 
@@ -61,8 +60,40 @@ def getValue(V,states):
         
     
     return val
-    
 
+# Using the two array version of policy evaluation, the value function is calculated
+def policyEvaluation_2Array(gridWorld,rewards,theta,discount):
+    gridMap , row , col = gridWorld.getEnvironmentMap()
+    V = np.zeros((row,col))
+    V_prev = np.zeros((row,col))
+    t = 0
+    
+    while True:
+        delta = 0
+        t += 1
+        for i in range(0,row):
+            for j in range(0,col):
+                val =  V[i,j]
+            
+                pi = 0.25
+                next_s = getNextStates(gridMap,i,j)
+                rew_s = getRewards(rewards,next_s)
+                
+                if (i,j) == (0,0):
+                    V[i,j] = 0
+                else:
+                    V[i,j] = np.sum(pi*(rew_s + np.multiply(discount,getValue(V_prev,next_s))))
+                
+                vDiff = abs(val-V[i,j])
+                delta = max(delta,vDiff)
+      
+        V_prev = V
+        if delta < theta:
+            break;
+    
+    return V,t
+    
+# Using the in place version of policy evaluation, the value function is calculated
 def policyEvaluation(gridWorld,rewards,theta,discount):
     gridMap , row , col = gridWorld.getEnvironmentMap()
     V = np.zeros((row,col))
@@ -70,71 +101,72 @@ def policyEvaluation(gridWorld,rewards,theta,discount):
     
     while True:
         delta = 0
-        #print(t)
         t += 1
+        
         for i in range(0,row):
             for j in range(0,col):
-                # print(i, "\n" ,j)
                 val =  V[i,j]
-                #print(val)
             
                 pi = 0.25
                 next_s = getNextStates(gridMap,i,j)
                 rew_s = getRewards(rewards,next_s)
-               
-                # print("next s  \n", next_s)
-                # print("reward\n", rew_s)
-                # print("v" , getValue(V,next_s))
-                # print(np.multiply(,getValue(V,next_s)))
-                # print(np.sum(pi*(rew_s + np.multiply(discount,getValue(V,next_s)))))
                 
                 if (i,j) == (0,0):
                     V[i,j] = 0
                 else:
                     V[i,j] = np.sum(pi*(rew_s + np.multiply(discount,getValue(V,next_s))))
-                #print(V[i,j])
+                
                 
                 vDiff = abs(val-V[i,j])
-                # print(vDiff)
-                # print(delta)
                 delta = max(delta,vDiff)
-                #print(delta)
       
         if delta < theta:
             break;
     
     return V,t
 
-# The creation of the grid world alongside the agents.
+# The creation of the grid world
 gridWorld = GridWorld(4,4)
 goal = np.array([0,0])
 
+# Creating the rewards function
 rewards = np.ones((4,4))*-1
 rewards[0,0] = 20
-print(rewards)
+# print(rewards)
 
 theta = 0.01
 discount = 1
 
-matrix,t = policyEvaluation(gridWorld,rewards,theta,discount)
-print(matrix)
-sns.heatmap(matrix)
+# Policy Evaluation using 2 array + heatmap
+matrix,t = policyEvaluation_2Array(gridWorld,rewards,theta,discount)
+# print("mat ",matrix)
+sns.heatmap(matrix, annot = True)
+plt.title('Two Array Policy Evaluation Heatmap')
+plt.savefig("2Arr_heatmap.png")
 plt.show()
 
-iterDF = pd.DataFrame(columns= ["discount", "iterations"])
+# Policy Evaluation using in place + heatmap
+matrix,t = policyEvaluation(gridWorld,rewards,theta,discount)
+# print("mat ",matrix)
+sns.heatmap(matrix, annot = True)
+plt.title('In-place Policy Evaluation Heatmap')
+plt.savefig("inplace_heatmap.png")
+plt.show()
+
+iterDF = pd.DataFrame()
 
 discounts = np.logspace(- 0.2, 0, num=20)
-print(discounts)
 for d in discounts:
-    #if d == 1:
-    #    break;
     matrix,t = policyEvaluation(gridWorld,rewards,theta,d)
-    iterDF = iterDF.append({"discount" : d, "iterations" : t}, ignore_index = True)
-    #sns.heatmap(matrix)
-    #plt.show()
-    #print(d)
-    #print(matrix)
-    #print(t)
+    matrix,t2 = policyEvaluation_2Array(gridWorld,rewards,theta,d)
+    iterDF = iterDF.append({"discount" : d, "iterations_inplace" : t, "iterations_2array" : t2}, ignore_index = True)
  
 display(iterDF)
-plt.plot('discount', 'iterations', data=iterDF)
+plt.plot('discount', 'iterations_inplace', data=iterDF)
+plt.plot('discount', 'iterations_2array', data=iterDF)
+plt.title('In-place vs Two Array Policy Evaluation')
+plt.xlabel('Discount')
+plt.ylabel('Number of Iterations')
+plt.legend()
+plt.savefig("2Arr_inplace_comparisons.png")
+plt.show()
